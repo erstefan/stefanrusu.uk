856b50a55cf338fe444bf21e5a9377c5735aa99e
Stefan Rusu <erstefan13@gmail.com>
2018-09-30 17:55:43 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+      <h1 class="title">
+        stefanrusu.uk
+      </h1>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
